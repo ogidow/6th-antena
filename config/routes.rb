3ae30cdc46ab1f74108c0f259fb9d885d523b57f
ogidow <syuta.ogido@gmail.com>
2016-06-01 17:41:14 +0900

    見た目簡単に作った

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :blogs
-  resources :blogs
-  get 'articles/index'
+  #resources :blogs
+  resources :articles
+  #get 'articles/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
