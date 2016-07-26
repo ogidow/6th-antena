0719d10d9608b843e371bd920d05797a7bf14fa0
ogidow <syuta.ogido@gmail.com>
2016-07-26 23:30:20 +0900

    レイアウト変更

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,8 +2,8 @@ class ArticlesController < ApplicationController
   def index
 	  @articles = Article.page(params[:page]).per(10).order(publish: :DESC)
 	  @articles.map do |article|
-		article.description = Sanitize.clean(article.description)
-		article
+		  article.description = Sanitize.clean(article.description)
+		  article
 	  end
 
   end
